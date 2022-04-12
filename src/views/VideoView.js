cfafb6963c59d776964b8ead19e81683949dedb7
Maksym Kozlov <m.kozlov@spd-ukraine.com>
2022-04-12 16:58:49 +0300

    change title on video section

--- src/views/VideoView.js
+++ src/views/VideoView.js
@@ -3,7 +3,7 @@ import { SectionWrap } from '../UI/SectionWrap';
 
 export const VideoView = () => {
   return (
-    <SectionWrap title="Contacts">
+    <SectionWrap title="Video">
       <Video />
     </SectionWrap>
   );
